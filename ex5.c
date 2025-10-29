87d58da2f7db3339dbd3d4f42b6364a3d0c47d67
Belisarius-159 <yusuforal7247@gmail.com>
2025-10-29 22:48:37 +0300

    Yusuf Oral's Update ex5.c

--- ex5.c
+++ ex5.c
@@ -1,1 +1,10 @@
+//Hello World, my name is Yusuf ORAL and this is my "c" exercise for YMT113 class
+//I use libraries for functions i will need
+#include <stdio.h>
+#include <cs50.h>
 
+int main(void) //This is my main function
+{
+	string answer = get_string("What is your name?\n ");  //I ask customers name
+	printf("Hello %s\n", answer); //We greet the customer
+}
