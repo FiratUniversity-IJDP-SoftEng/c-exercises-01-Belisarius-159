f56fe57325a5c791dd0fd51f7d95905e90aad59c
Belisarius-159 <yusuforal7247@gmail.com>
2025-10-30 00:11:23 +0300

    Yusuf ORAL's Update ex3.c

--- ex3.c
+++ ex3.c
@@ -1,2 +1,24 @@
+//Hello World, my name is Yusuf ORAL and this is my "c" exercise for YMT113 class
+//I use libraries for functions i will need
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string pet = "cat";
+    string say = get_string("Do you have dog or cat?\n");
+
+    if (say == pet)
+    {
+        printf("meow\n");
+    }
+    else
+    {
+        printf("woof\n");
+    }
+    return 0;
+}
+//Code is malfunctioning and I don't know how to fix.
+//I would like to take your advise on that in your free time sir.
 
 
